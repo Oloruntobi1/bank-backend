92f500f96449f8beb8f0a25f0b8d47f2ddac1784
Oloruntobi1 <blgntobi@gmail.com>
2020-11-07 19:27:17 +0100

    implemented test for delete user

--- db/sqlc/user_test.go
+++ db/sqlc/user_test.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/Oloruntobi1/Oloruntobi1/bank_backend/util"
@@ -49,4 +50,42 @@ func TestGetUser(t *testing.T) {
 	require.Equal(t, user.Password, gottenUser.Password)
 	
 
+}
+
+func TestListUsers(t *testing.T) {
+
+	for i := 0; i < 10 ; i++ {
+		createRandomUser(t)
+	}
+
+	arg := ListUsersParams {
+		Limit: 5,
+		Offset: 5,
+	}
+
+	users, err := testQueries.ListUsers(context.Background(), arg)
+
+	require.NoError(t, err)
+
+	require.Len(t, users, 5)
+
+	for _, user := range users {
+		require.NotEmpty(t, user)
+	}
+
+}
+
+func TestDeleteUser(t *testing.T) {
+
+	user := createRandomUser(t)
+
+	err := testQueries.DeleteUser(context.Background(), user.ID)
+
+	require.NoError(t, err)
+
+	notGottenUser, err := testQueries.GetUser(context.Background(), user.ID)
+
+	require.Error(t, err)
+	require.EqualError(t, err, sql.ErrNoRows.Error())
+	require.Empty(t, notGottenUser)
 }
